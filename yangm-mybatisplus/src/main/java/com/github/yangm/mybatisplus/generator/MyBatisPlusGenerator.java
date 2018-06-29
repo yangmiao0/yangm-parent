38813e5c0900d83dbddf2150da52a2e1d4f965b9
Administrator <Administrator@192.168.199.186>
2018-06-29 17:03:45 +0800

    mybatisplus

--- yangm-mybatisplus/src/main/java/com/github/yangm/mybatisplus/generator/MyBatisPlusGenerator.java
+++ yangm-mybatisplus/src/main/java/com/github/yangm/mybatisplus/generator/MyBatisPlusGenerator.java
@@ -20,101 +20,94 @@ import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.DbType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
-/**   
+/**
  * 
- * 功能描述：代码生成器 包括service和controller
- * @Package: com.github.yangm.mybatisplus.util 
+ * 功能描述：代码生成器 包括自动生成service和controller 完成后刷新项目
+ * 
+ * @Package: com.github.yangm.mybatisplus.util
  * @author: yangm
- * @date: 2018年6月28日 上午10:13:33 
+ * @date: 2018年6月28日 上午10:13:33
  * @email：1169290944@qq.com
  */
 public class MyBatisPlusGenerator {
 
 	/**
-	 * 代码生成    示例代码
+	 * 代码生成 示例代码
 	 */
 	@Test
-	public void  myBatisPlusGenerator() {
-		
-		//可多个表名
-		String[] tableNames = {"car_info"};
-		
+	public void myBatisPlusGenerator() {
+
+		// 可多个表名
+		String[] tableNames = { "car_info" };
+
 		String outputDir = "D:\\eclipse-workspace3\\yangm-parent\\yangm-mybatisplus";
-		
-		//1. 全局配置
+
+		// 1. 全局配置
 		GlobalConfig config = new GlobalConfig();
 		config.setActiveRecord(false) // 是否支持AR模式
-			  .setAuthor("yangm") // 作者
-			  .setOutputDir(outputDir + "\\src\\main\\java") // 生成路径
-			  .setFileOverride(true)  // 文件覆盖
-			  .setIdType(IdType.AUTO) // 主键策略
-			  .setServiceName("%sService")  // 设置生成的service接口的名字的首字母是否为I
-			  .setEnableCache(false)// XML 二级缓存
- 			  .setBaseResultMap(true)// XML ResultMap
- 			  .setBaseColumnList(true);// XML columList
-		
-		//2. 数据源配置
-		DataSourceConfig  dsConfig  = new DataSourceConfig();
-		dsConfig.setDbType(DbType.MYSQL)  // 设置数据库类型
-				.setDriverName("com.mysql.jdbc.Driver")
-				.setUrl("jdbc:mysql://localhost:3306/test")
-				.setUsername("root")
-				.setPassword("123456");
-		 
-		//3. 策略配置
-		StrategyConfig stConfig = new StrategyConfig();
-		stConfig.setCapitalMode(true) //全局大写命名
-				.setDbColumnUnderline(true)  // 指定表名 字段名是否使用下划线
-				.setNaming(NamingStrategy.underline_to_camel) // 数据库表映射到实体的命名策略
-				//.setSuperControllerClass("com.baomidou.demo.TestController")//自定义 controller 父类
-				//.setTablePrefix("tbl_")	// 表前缀
-				.setInclude(tableNames);  // 生成的表
-		
-		//4. 包名策略配置 
-		PackageConfig pkConfig = new PackageConfig();
-		pkConfig.setParent("com.github.yangm.mybatisplus")
-				.setMapper("dao")
-				.setService("service")
-				.setController("controller")
-				//.setXml("dao.mapper")
-				.setEntity("beans");
-		
-        // 注入自定义配置，可以在 VM 中使用 cfg.abc 【可无】
-        InjectionConfig cfg = new InjectionConfig() {
-            @Override
-            public void initMap() {
-                Map<String, Object> map = new HashMap<String, Object>();
-                map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
-                this.setMap(map);
-            }
-        };
+				.setOpen(false).setAuthor("yangm") // 作者
+				.setOutputDir(outputDir + "\\src\\main\\java") // 生成路径
+				.setFileOverride(true) // 文件覆盖
+				.setIdType(IdType.AUTO) // 主键策略
+				.setServiceName("%sService") // 设置生成的service接口的名字的首字母是否为I
+				.setEnableCache(false)// XML 二级缓存
+				.setBaseResultMap(true)// XML ResultMap
+				.setBaseColumnList(true);// XML columList
 
-        // 自定义 mapper 生成路径
-        List<FileOutConfig> focList = new ArrayList<FileOutConfig>();
-        focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                // 自定义输入文件名称
-                return outputDir + "\\src\\main\\resources\\mapping\\" + tableInfo.getEntityName() + "Mapper.xml";
-            }
-        });
-        cfg.setFileOutConfigList(focList);
-        
-        // 关闭默认 xml 生成，调整生成 至 根目录
-        TemplateConfig tc = new TemplateConfig();
-        tc.setXml(null);
-        
-		//5. 整合配置
-		AutoGenerator  ag = new AutoGenerator();
-		ag.setGlobalConfig(config)
-		  .setDataSource(dsConfig)
-		  .setStrategy(stConfig)
-		  .setPackageInfo(pkConfig)
-		  .setCfg(cfg)
-		  .setTemplate(tc);
-		
-		//6. 执行
+		// 2. 数据源配置
+		DataSourceConfig dsConfig = new DataSourceConfig();
+		dsConfig.setDbType(DbType.MYSQL) // 设置数据库类型
+				.setDriverName("com.mysql.jdbc.Driver").setUrl("jdbc:mysql://localhost:3306/test").setUsername("root")
+				.setPassword("123456");
+
+		// 3. 策略配置
+		StrategyConfig stConfig = new StrategyConfig();
+		stConfig.setCapitalMode(true) // 全局大写命名
+				.setDbColumnUnderline(true) // 指定表名 字段名是否使用下划线
+				.setNaming(NamingStrategy.underline_to_camel) // 数据库表映射到实体的命名策略
+				// .setSuperControllerClass("com.baomidou.demo.TestController")//自定义 controller父类
+				// .setTablePrefix("tbl_") // 全局表前缀
+				.setInclude(tableNames); // 生成的表
+
+		// 4. 包名策略配置
+		PackageConfig pkConfig = new PackageConfig();
+		pkConfig.setParent("com.github.yangm.mybatisplus").setMapper("dao").setService("service")
+				.setController("controller")
+				// .setXml("dao.mapper")
+				.setEntity("beans");
+
+		// 注入自定义配置，可以在 VM 中使用 cfg.abc 【可无】
+		InjectionConfig cfg = new InjectionConfig() {
+			@Override
+			public void initMap() {
+				Map<String, Object> map = new HashMap<String, Object>();
+				map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
+				this.setMap(map);
+			}
+		};
+
+		// 自定义 mapper 生成路径
+		List<FileOutConfig> focList = new ArrayList<FileOutConfig>();
+		focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
+			@Override
+			public String outputFile(TableInfo tableInfo) {
+				// 自定义输入文件名称
+				return outputDir + "\\src\\main\\resources\\mapping\\" + tableInfo.getEntityName() + "Mapper.xml";
+			}
+		});
+		cfg.setFileOutConfigList(focList);
+
+		// 关闭默认 xml 生成，调整生成 至 根目录
+		TemplateConfig tc = new TemplateConfig();
+		tc.setXml(null);
+
+		// 5. 整合配置
+		AutoGenerator ag = new AutoGenerator();
+		ag.setGlobalConfig(config).setDataSource(dsConfig).setStrategy(stConfig).setPackageInfo(pkConfig).setCfg(cfg)
+				.setTemplate(tc);
+
+		// 6. 执行
 		ag.execute();
 	}
-	
+
 }
